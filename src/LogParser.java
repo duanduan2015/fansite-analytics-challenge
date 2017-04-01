0efb69330dcad076118aea92a2f8d63b3ba713dc
Yunduan Han <yunduan.han@gmail.com>
2017-04-01 11:34:15 -0700

    LogParser basic function ok

--- src/LogParser.java
+++ src/LogParser.java
@@ -1,3 +1,11 @@
+import java.io.*;
 public class LogParser {
-    
+   public LogParser(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
+        String line = br.readLine();
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         String inputPath = args[0];
-        BufferedReader br = new BufferedReader(new FileReader(new File(inputPath)));
-        String line = br.readLine();
-        String[] ss = line.split(" ");
-        for (String s : ss) {
-            System.out.println(s);
-        }
-        String s = "/home/dis/duanduan";
-        ss = s.split("/");
-        for (String sss : ss) {
-            System.out.println(sss);
-        }
-
-        System.out.println(ss[0].matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"));
-        
-        /*while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }*/
-        br.close();
+        LogParser parser = new LogParser(inputPath);
     }
 }
