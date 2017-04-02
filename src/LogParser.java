7109e9dd64a1ea953f299a73b0ac649c3243e965
Yunduan Han <yunduan.han@gmail.com>
2017-04-01 18:43:03 -0700

    Regular expression works ok in LogParser class

--- src/LogParser.java
+++ src/LogParser.java
@@ -1,11 +1,47 @@
 import java.io.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class LogParser {
-   public LogParser(String path) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-        String line = br.readLine();
-        while ((line = br.readLine()) != null) {
+
+    private BufferedReader bufferedReader;
+    private static Pattern REGEX_PATTERN;
+
+    public LogParser(String path) throws IOException {
+        bufferedReader = new BufferedReader(new FileReader(new File(path)));
+    }
+
+    public void parse() throws IOException {
+        String line = null;
+        while ((line = bufferedReader.readLine()) != null) {
             System.out.println(line);
+            Matcher m = REGEX_PATTERN.matcher(line);
+            if (m.find()) {
+                System.out.printf("%s,%s,%s,%s,%s,%s,%s\n", m.group(1), 
+                        m.group(2), m.group(3), m.group(4), m.group(5), m.group(6), m.group(7));
+            } else {
+                throw new IllegalArgumentException("Unable to parse line '" + line + "'");
+            }
         }
-        br.close();
-   }
+        bufferedReader.close();
+    }
+
+    private static void buildRegexPattern() {
+        String reClientAddr = "(\\S+)";
+        String reDoubleDashed = "-\\s+-";
+        String reTimeStamp = "\\[(.+)\\]";
+        String reHTTPRequest = "[\"“](\\S+)\\s(\\S+)(?:\\s+(\\S+))?[\"”]";
+        String reHTTPResponse = "(\\d+)\\s([\\d-]+)";
+        String assembled = "^\\s*" + reClientAddr +
+            "\\s*" + reDoubleDashed + 
+            "\\s*" + reTimeStamp + 
+            "\\s*" + reHTTPRequest + 
+            "\\s*" + reHTTPResponse +
+            "\\s*$";
+        REGEX_PATTERN = Pattern.compile(assembled);
+    }
+
+    static {
+        buildRegexPattern();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String inputPath = args[0];
         LogParser parser = new LogParser(inputPath);
+        parser.parse();
     }
 }
