eca1d3caa12b5985917fc9327c45673dc2758db7
Yunduan Han <yunduanh@usc.edu>
2017-04-05 15:30:29 -0700

    JUnit for IPv4Address to eliminate bad IP address

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -25,4 +25,18 @@ public class UnitTest {
         Assert.assertEquals(Arrays.asList("asdf", ""), (new ResourcePath("/asdf/")).getSegments());
         Assert.assertEquals(Arrays.asList("  a", "b  c", ""), (new ResourcePath("/  a/b  c/")).getSegments());
     }
+
+    @Test
+    public void testIPv4Address() {
+        Assert.assertEquals("0.0.0.0", (new IPv4Address("0.0.0.0")).toString());
+        Assert.assertEquals("100.100.100.100", (new IPv4Address("100.100.100.100")).toString());
+
+        for (String s: Arrays.asList("256.0.0.0", "0", "", "0.0.0.-1", "hello.world.1.0")) {
+            try {
+                IPv4Address dummy = new IPv4Address(s);
+                Assert.fail("IPv4Address('" + s + "') should throw " + IllegalArgumentException.class.getCanonicalName());
+            } catch (IllegalArgumentException e) {}
+        }
+    }
+
 }
--- src/tools/IPv4Address.java
+++ src/tools/IPv4Address.java
@@ -6,20 +6,40 @@ public class IPv4Address implements Comparable<IPv4Address> {
     private Integer integerAddress;
     public IPv4Address (String s) {
         String[] parts = s.split("\\.");
-        this.integerAddress = 0;
-        for (int i = 0; i < parts.length; i++) {
-            int power = 3 - i;
-            int ip = Integer.parseInt(parts[i]);
-            this.integerAddress += ip * (int)(Math.pow(256, power));
+        if (parts.length != 4) {
+            throw new IllegalArgumentException("Bad IPv4 address " + s);
         }
+
+        int n = 0;
+        for (int i = 0; i < parts.length; i++) {
+            int ip = Integer.parseInt(parts[i]);
+            if ((ip | 0xff) != 0xff) {
+                throw new IllegalArgumentException("Bad IPv4 Address " + s);
+            }
+            n <<= 8;
+            n |= ip; 
+        }
+
+        this.integerAddress = n;
     }
     
+    @Override
     public int compareTo(IPv4Address ip) {
         return this.integerAddress - ip.getIPValue();
     }
 
     public Integer getIPValue() {
         return this.integerAddress;
+    }
+
+    public String toString() {
+        String result = "";
+        for (int i = 3; i >= 0; --i) {
+            result = result + Integer.toString((this.integerAddress >>> i * 8) & 0xff);
+            if (i <= 0) continue;
+            result = result + ".";
+        }
+        return result;
     }
 }
 
